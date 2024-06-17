dd10a43fd2497acbad610efb764eb66bf140fabf
Priscila Lopez <prisscilalb@gmail.com>
2024-06-17 15:49:37 -0400

    test

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-int main(void){
-/*test*/
-
-	puts("Programming is like building a multilingual puzzle");
-
-	return 0;
+/**
+ * main - Program entry point
+ * Return:  0 on success. Error code otherwise
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
