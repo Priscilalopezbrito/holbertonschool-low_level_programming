e35f7cbaac8193791003ee1703bd16627a280ba8
Priscila Lopez <prisscilalb@gmail.com>
2024-06-17 16:17:16 -0400

    test

---
+++ hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ * Return:  0 on success. Error code otherwise
+ */
+int main(){
+sizeof(char);
+sizeof(int);
+sizeof(long int);
+sizeof(long long int);
+sizeof(float);
+return (0);
+ }
