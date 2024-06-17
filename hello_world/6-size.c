679d0b3a3b10444ed6535e9b1e52eaa52407a6bb
Priscila Lopez <prisscilalb@gmail.com>
2024-06-17 19:26:20 -0400

    l

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,12 +4,12 @@
  *@a-printf to format byte size
  * Return:  0 on success. Error code otherwise
  */
-int main()
+int main(void)
 {
-printf("Size of a char: %zu byte(s)\n", sizeof(char));
-printf("Size of an int: %zu byte(s)\n", sizeof(int));
-printf("Size of a long int: %zu byte(s)\n", sizeof(long int);
-printf("Size of a long long int: %zu byte(s)\n ", sizeof(long long int));
-printf("Size of a float: %zu byte(s)\n", sizeof(float));
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int);
+printf("Size of a long long int: %lu byte(s)\n ", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
