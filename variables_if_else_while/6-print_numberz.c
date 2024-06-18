6a62f2b81397c12fc7fc0f0df77d020707cdac50
Priscila Lopez <prisscilalb@gmail.com>
2024-06-18 16:00:41 -0400

    test

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ * Return:  0 on success. Error code otherwise
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		printf("%d", i);
+	}
+	putchar ('\n');
+	return (0);
+}
