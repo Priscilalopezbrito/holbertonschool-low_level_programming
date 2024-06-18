208e14ee136a5fd37e8de61422222e87055fc26a
Priscila Lopez <prisscilalb@gmail.com>
2024-06-18 16:39:58 -0400

    testing question 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ * Return:  0 on success. Error code otherwise
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 122 ; i <= 97  ; i++)
+	{
+		putchar(i);
+	}
+	putchar ('\n');
+	return (0);
+}
