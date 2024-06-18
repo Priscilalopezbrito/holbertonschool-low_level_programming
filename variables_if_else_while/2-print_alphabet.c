4294725cd71cd9a90eff8de4ed385ebe688a310c
Priscila Lopez <prisscilalb@gmail.com>
2024-06-18 13:07:28 -0400

    test

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ * Return:  0 on success. Error code otherwise
+ */
+int main(void)
+{
+	char i;
+	for (i = 97 ; i <= 122 ; i++)
+	{
+		putchar (i);
+	}
+	return (0);
+}
