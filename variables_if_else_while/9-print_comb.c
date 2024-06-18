cb43da0fc7b665c244fd3cc45a52c7c384119d7c
Priscila Lopez <prisscilalb@gmail.com>
2024-06-18 17:40:25 -0400

    c file 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ * Return:  0 on success. Error code otherwise
+ */
+int main(void)
+{
+	char i;
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		putchar(i);
+		putchar(44);
+		putchar(32);
+	}
+	putchar ('\n');
+	return (0);
+}
