022b3f8f40fdf8bd115d17fd454a7c208d71b64a
Priscila Lopez <prisscilalb@gmail.com>
2024-06-19 11:28:08 -0400

    betty fix file 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - Program entry point
+ * Return:  0 on success. Error code otherwise
+ */
+int main(void)
 {
-char str[]="_putchar";
+char str[] = "_putchar";
 int i;
 
-		for( i = 0; i < 8; i++)
+		for (i = 0; i < 8; i++)
 		{
 			putchar (str[i]);
 		}
-	
 	putchar ('\n');
 	return (0);
-}	
+}
