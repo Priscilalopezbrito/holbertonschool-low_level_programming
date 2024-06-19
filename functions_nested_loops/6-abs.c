301ce661ca36b5516e53e386b852901b06574f1a
Priscila Lopez <prisscilalb@gmail.com>
2024-06-19 19:31:10 -0400

    test file 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - looks for lowercase character
+ * @n: int to check
+ *
+ * Description: calculate abs
+ *
+ * Return: returns 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+		return (n);
+}
