4d79d203e5afff026bdcf6bc8b553a93ba37b6ae
Priscila Lopez <prisscilalb@gmail.com>
2024-06-19 09:37:40 -0400

    test

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - Program entry point
+ * Return:  0 on success. Error code otherwise
+ */
+int main(void)
+{
+	char i = "_putchar";
+
+putchar(i);
+	return (0);
+}	
