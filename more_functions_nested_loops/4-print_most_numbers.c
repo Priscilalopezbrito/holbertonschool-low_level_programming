38e19f3091a35795d3ccbaba7d78daa0a2714dcb
Priscila Lopez <prisscilalb@gmail.com>
2024-06-20 11:22:37 -0400

    file 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints num excluding two digits
+ * Description: numbers 0 to 9  minus 2 and 4
+ */
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'  ; i <= '9' ; i++)
+	{
+		if (i != '2' &&  i != '4')
+		{
+		_putchar (i);
+		}
+	}
+	_putchar ('\n');
+}
