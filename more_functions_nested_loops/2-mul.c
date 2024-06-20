f6785b4e9547dab0fbd09d1d338a419e4a0bb37f
Priscila Lopez <prisscilalb@gmail.com>
2024-06-20 09:52:13 -0400

    test

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *mul - looks for lowercase character
+ *@a: first number
+ *@b: second number
+ * Description: mul
+ *
+ * Return: returns 0
+ */
+int mul(int a, int b)
+{
+	int mul = a * b;
+
+	return (mul);
+}
