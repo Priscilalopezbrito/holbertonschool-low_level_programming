22486739d05f7dec389453e5b5d00a544c4835ea
Priscila Lopez <prisscilalb@gmail.com>
2024-06-19 22:20:50 -0400

    k

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,30 +8,30 @@
  */
 void times_table(void)
 {
-	int a;
-	int b;
+	int a, b;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0 ; a < 10 ; a++)
 	{
-		for (b = 0; b < 10; b++)
+		for (b = 0 ; b < 10 ; b++)
 		{
 			int c = a * b;
 
-			if (c < 10)
-			{
-				_putchar(' ');
-				_putchar('0' + c);
-			}
-			else
-			{
-				_putchar('0' + c / 10);
-				_putchar('0' + c % 10);
-			}
-			if (b < 9)
+			if (b != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
+
+			if (c < 10 && b != 0)
+			{
+				_putchar(' ');
+			}
+
+			if (c >= 10)
+			{
+				_putchar('0' + c / 10);
+			}
+			_putchar('0' + c % 10);
 		}
 		_putchar('\n');
 	}
