ff9a1dfad40f93862e5fb08313b2d500f37b2a58
Priscila Lopez <prisscilalb@gmail.com>
2024-06-19 21:26:32 -0400

    test file 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * times_table - 9 times 9
+ *
+ * Description: prints mult table
+ *
+ * Return: returns 0
+ */
+void times_table(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			int c = a * b;
+
+			if (c < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + c);
+			}
+			else
+			{
+				_putchar('0' + c / 10);
+				_putchar('0' + c % 10);
+			}
+
+			if (b < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
