43e3d160bc5a37f1c7712163534b1f71e6cbda59
Priscila Lopez <prisscilalb@gmail.com>
2024-06-19 20:29:18 -0400

    test file 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *add - looks for lowercase character
+ *@n1: first number
+ *@n2: second number
+ * Description: sum
+ *
+ * Return: returns 0
+ */
+int add(int n1, int n2)
+{
+	int sum = n1 + n2;
+
+	return (sum);
+}
