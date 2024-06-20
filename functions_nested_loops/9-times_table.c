4dfb7dc6800cd26bf569ef1f7dc0d9fba7db9dbe
Priscila Lopez <prisscilalb@gmail.com>
2024-06-19 21:38:42 -0400

    test

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -11,9 +11,9 @@ void times_table(void)
 	int a;
 	int b;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = 0; b < 10; b++)
 		{
 			int c = a * b;
 
