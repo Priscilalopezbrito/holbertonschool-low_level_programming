63d9018935a44d3e12e0145eafb4882b1ac01719
Priscila Lopez <prisscilalb@gmail.com>
2024-06-20 19:13:31 -0400

    update file 7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -7,16 +7,22 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i, s;
 
-	for (i = 0 ; i < n ; i++)
+	if (n <= 0)
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
-		else
-		_putchar(92);
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		for (i = 0 ; i < n ; i++)
+		{
+			for (s = 0 ; s < i ; s++)
+			{
+				_putchar(32);
+			}
+		_putchar(92);
+		_putchar('\n');
+		}
+	}
 }
