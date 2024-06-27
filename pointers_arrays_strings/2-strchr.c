7751a3958256053d78a57791a7ab1f19a7994eab
Priscila Lopez <prisscilalb@gmail.com>
2024-06-27 17:19:45 -0400

    file 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_strchr- function that locates a character in a string.
+ *@s: char ptr *s
+ *@c: char c
+ *Return: pointer to dest
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (0);
+}
+
