deca2a545db7ca4da17067686ea17cc796c99184
Priscila Lopez <prisscilalb@gmail.com>
2024-06-27 13:48:42 -0400

    file 0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_memset- fills memory with a constant byte.
+ *@s: char ptr
+ *@b: char ptr
+ *@n: uns int
+ *Return: pointer ro rhe memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int c;
+
+	for (c = 0 ; c < n ; c++)
+	{
+		s[c] = b;
+		c++;
+	}
+	return (s);
+}
