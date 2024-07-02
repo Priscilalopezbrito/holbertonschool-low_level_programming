9c330cad15e0083475c6d65029155c2575ef6207
Priscila Lopez <prisscilalb@gmail.com>
2024-07-02 16:23:45 -0400

    condition added to file 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -13,10 +13,9 @@ int main(int argc, char *argv[])
 	int index;
 	int add;
 
-	if (argc == 1)/*if theres no number print 0*/
+	if (argc < 2)/*if theres no number print 0*/
 	{
 		printf("0\n");
-		return (0);
 	}
 	add = 0;
 	for (index = 1; index < argc; index++)
@@ -28,6 +27,9 @@ int main(int argc, char *argv[])
 		}
 		add = add + atoi(argv[index]);/**/
 	}
-	printf("%d\n", add);
+	if (argc > 1)
+	{
+		printf("%d\n", add);
+	}
 	return (0);
 }
