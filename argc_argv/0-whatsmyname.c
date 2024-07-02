e40416496597e5dc6edb5665d74fd9b754eba80f
Priscila Lopez <prisscilalb@gmail.com>
2024-07-02 12:44:36 -0400

    file 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main- prints its name
+ *@argc: count
+ *@argv: array
+ *Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	printf("\"%s\"\n", argv[i]);
+	}
+	return (0);
+}
