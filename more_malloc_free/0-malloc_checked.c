e78dd7241531e2de364ce7f00834c3d401861e5f
Priscila Lopez <prisscilalb@gmail.com>
2024-07-04 11:41:28 -0400

    file 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *malloc_checked- allocates memory using malloc
+ *@b: int
+ *Return: Returns a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *arr;
+
+	/*allocates memory using malloc*/
+	arr = (int *)malloc(b * sizeof(int));
+	/*if malloc fails, the malloc_checked
+	 * function should cause normal process
+	 * termination with a status value of 98*/
+	if (arr == NULL)
+	{
+		exit(98);
+	}
+	/*Returns a pointer to the allocated memory*/
+	return (arr);
+}
