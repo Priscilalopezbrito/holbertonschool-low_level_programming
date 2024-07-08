42af8fb2f0b10a491161a5235d1a8a2777e4358e
Priscila Lopez <prisscilalb@gmail.com>
2024-07-08 18:02:23 -0400

    test

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,6 +14,4 @@ float age;
 #endif
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+
