15e78e1137390bd3108f179a1884cc5cc69263a3
Priscila Lopez <prisscilalb@gmail.com>
2024-07-08 20:57:10 -0400

    test

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -17,15 +17,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	new_dog->name = (char *)malloc(sizeof(name) + 1);
-	if (new_dog->name == NULL)
-	{
-		return (NULL);
-	}
 	new_dog->age = age;
 	new_dog->owner = (char *)malloc(sizeof(owner) + 1);
-	if (new_dog->owner == NULL)
-	{
-		return (NULL);
-	}
 	return (new_dog);
 }
