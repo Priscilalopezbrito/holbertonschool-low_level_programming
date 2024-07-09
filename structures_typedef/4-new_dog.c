08d421d4fe3e394fb9832cbda082cc87c03dd577
Priscila Lopez <prisscilalb@gmail.com>
2024-07-08 21:21:36 -0400

    test

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include "dog.h"
+char *_strcpy(char *dest, char *src);
+
 /**
  *new_dog- Creates a new dog
  *@name: Name
@@ -16,8 +18,51 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	new_dog->name = name;
+	/**/
+	new_dog->name = malloc(sizeof(name) + 1);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	_strcpy(new_dog->name, name);
+	/**/
 	new_dog->age = age;
-	new_dog->owner = owner;
+	/**/
+	new_dog->owner = malloc(sizeof(owner) + 1);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	_strcpy(new_dog->owner, owner);
 	return (new_dog);
+}
+
+
+
+
+
+
+
+
+/**
+ **_strcpy - copies string pointed to by src
+ *@dest: ptr
+ *@src: ptr
+ *Return: ptr
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (ptr);
 }
