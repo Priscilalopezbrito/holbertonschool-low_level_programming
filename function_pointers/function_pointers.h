21143fbbf02805369057d0883472b7ee43158e44
Priscila Lopez <prisscilalb@gmail.com>
2024-07-10 10:11:10 -0400

    test

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,10 @@
+#ifndef FP
+#define FP
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
+/*void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
+int (*get_op_func(char *s))(int, int);*/
+void pname(char *name);
 
+#endif
