e368c09ab9706fc620c681942215591a92671791
Priscila Lopez <prisscilalb@gmail.com>
2024-07-13 22:47:10 -0400

    test

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
 	int num1;
 	int num2;
+	int calc;
 	char operator;
 
 	if (argc != 4)
@@ -16,8 +17,10 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	operator = argv[2][0];
-
-
+	
+/* the result of all operations can be stored in an int*/
+	calc = (get_op_func(argv[2])(num1, num2));
+	printf("%d\n",calc);
 /*if the operator is none of the above, print Error*/
 	if (operator != '+' && operator != '-' && operator != '*' && operator != '/' && operator != '%')
 	{
