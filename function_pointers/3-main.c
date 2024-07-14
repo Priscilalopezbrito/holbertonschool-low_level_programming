0cf19633d4cc0bfe532a0e40bff7433b02e5edc0
Priscila Lopez <prisscilalb@gmail.com>
2024-07-14 12:18:27 -0400

    fix file main

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 	int num1;
 	int num2;
 	int calc;
-	char operator;
+	char *operator;
 
 	if (argc != 4)
 	{
@@ -21,14 +21,14 @@ int main(int argc, char *argv[])
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	operator = argv[2][0];
+	operator = argv[2];
 /* the result of all operations can be stored in an int*/
 	calc = (get_op_func(argv[2])(num1, num2));
 	printf("%d\n", calc);
 /*if the operator is none of the above, print Error*/
-	if (operator != '+' && operator != '-' &&
-		operator != '*' && operator != '/' &&
-		operator != '%')
+	if (operator[0] != '+' && operator[0] != '-' &&
+		operator[0] != '*' && operator[0] != '/' &&
+		operator[0] != '%')
 	{
 		printf("Error\n");
 		exit(99);
