54f5ae06ba8594f822b8aa98d4b76bb124907249
Priscila Lopez <prisscilalb@gmail.com>
2024-07-23 21:15:32 -0400

    Fixing betty style file 1-list_len.c

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-
+/**
+ * list_len-  function returns the number of elements in a linked list_t list
+ * @h: head
+ * Return: number of elements
+ */
 size_t list_len(const list_t *h)
 {
 	int count;
