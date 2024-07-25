8787f0f06d7407596dd3af4dfdab722fc9833811
Priscila Lopez <prisscilalb@gmail.com>
2024-07-25 09:19:55 -0400

    adding free to conditio

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 /**
  * add_node- adds new node at the beginning of list_t list
- * @head:
- * @str:
+ * @head: head node
+ * @str:str char *
  * Return: address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	unsigned int i;
-	list_t *new = malloc(sizeof(list_t));/*creating space for new node*/
-	if (new == NULL)
+	list_t *newNode = malloc(sizeof(list_t));/*creating space for new node*/
+
+	if (newNode == NULL)
 	{
+		free (newNode);
 		return (NULL);
 	}
-	else
-	{
-		new->str =  (char*)str;
-		new->next = NULL;
+		newNode->str = strdup(str);
+		/*new->len = len;*/
+		newNode->next = NULL;
+
 		for (i = 0; str[i] != '\0'; i++)
 		{
-			new->len = i + 1;
+			newNode->len = i + 1;
 		}
-		new->next = *head;/*updates link part of the node*/
-		*head = new;/*points to new node*/
-	}
-	return (new);
+		newNode->next = *head; /*head;*updates link part of the node*/
+		*head = newNode;/*points to new node*/
+
+	return (newNode);
 }
