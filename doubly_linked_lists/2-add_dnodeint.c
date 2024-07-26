2cd831c5f64f689ea3dcbddb4889e2de5e148bcc
Priscila Lopez <prisscilalb@gmail.com>
2024-07-26 15:22:40 -0400

    fix betty style

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,37 +1,27 @@
-#include <stdio.h>
+i#include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 /**
- * dlistint_t- adds new node at the beginning of dlistint_t list
- * @head:
- * @n:
+ * add_dnodeint- adds new node at the beginning of dlistint_t list
+ * @head: head
+ * @n:num
  * Return: the adress of a new element of NULL if it failed
  **/
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t * newNode = malloc(sizeof(dlistint_t));
+	dlistint_t *newNode = malloc(sizeof(dlistint_t));
 
 	if (newNode == NULL)
 	{
 		return (NULL);
 	}
-
-	/*condition in nothing passed to n*/
 	newNode->prev = NULL ;/*???*/
 	newNode->n = n;
 	newNode->next = *head;
-
-	/*if *head null??
-	 * */
-	/*verify head???*/
-	/*si head no es null 
-	 * */
 	if (*head != NULL)
 	{
 		(*head)->prev = newNode;
 	}
-
-	/*(*head)->prev = newNode;*//*checking...*/
 	*head = newNode;
 	return (*head);
 
