97d7f581ba8d2794692c9052d0bbde2112eba5ac
Priscila Lopez <prisscilalb@gmail.com>
2024-08-22 01:10:33 -0400

    test

---
+++ file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "main.h"
+
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t n_written;
+	size_t len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[len] != '\0')
+			len++;
+		n_written = write(fd, text_content, len);
+		if (n_written == -1 || (size_t)n_written != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+    close(fd);
+    return (1);
+}
+
