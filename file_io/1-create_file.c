702cc9ae262eef5be024568ecf62fac323816147
Priscila Lopez <prisscilalb@gmail.com>
2024-08-22 01:13:05 -0400

    test

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -5,7 +5,12 @@
 #include <fcntl.h>
 #include "main.h"
 
-
+/**
+ * create_file- create file
+ * @filename: filaname
+ * @text_content: text content
+ * Return: 1
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
@@ -28,7 +33,7 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		}
 	}
-    close(fd);
-    return (1);
+	close(fd);
+	return (1);
 }
 
