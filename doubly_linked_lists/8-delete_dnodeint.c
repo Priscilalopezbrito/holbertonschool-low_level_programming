c1bb4750409efb494d620aec9a84ee049e159f13
Priscila Lopez <prisscilalb@gmail.com>
2024-08-22 01:50:45 -0400

    betty

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -9,36 +9,31 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *temp = *head;
-    unsigned int i;
+	dlistint_t *temp = *head;
+	unsigned int i;
 
-    if (*head == NULL)
-        return (-1);
-
-    /* If the node to delete is the head */
-    if (index == 0)
-    {
-        *head = temp->next;
-        if (temp->next != NULL)
-            temp->next->prev = NULL;
-        free(temp);
-        return (1);
-    }
-
-    /* Traverse the list to find the node to delete */
-    for (i = 0; temp != NULL && i < index; i++)
-        temp = temp->next;
-
-    /* If the node doesn't exist */
-    if (temp == NULL)
-        return (-1);
-
-    /* Unlink the node from the list */
-    if (temp->next != NULL)
-        temp->next->prev = temp->prev;
-    if (temp->prev != NULL)
-        temp->prev->next = temp->next;
-
-    free(temp);
-    return (1);
+	if (*head == NULL)
+		return (-1);
+	/* If the node to delete is the head */
+	if (index == 0)
+	{
+		*head = temp->next;
+		if (temp->next != NULL)
+			temp->next->prev = NULL;
+		free(temp);
+		return (1);
+	}
+	/* Traverse the list to find the node to delete */
+	for (i = 0; temp != NULL && i < index; i++)
+		temp = temp->next;
+	/* If the node doesn't exist */
+	if (temp == NULL)
+		return (-1);
+	/* Unlink the node from the list */
+	if (temp->next != NULL)
+		temp->next->prev = temp->prev;
+	if (temp->prev != NULL)
+		temp->prev->next = temp->next;
+	free(temp);
+	return (1);
 }
